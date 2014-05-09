2803132723435385a2e3de6b3143681895d44dd8
Andrew Hammond <andrew@evertrue.com>
2014-05-09 12:18:46 -0400

    Switch to single suite rather than bulk. Implement rscout.yml config standard

--- lib/rscout.rb
+++ lib/rscout.rb
@@ -1,5 +1,4 @@
 require 'rscout/version'
-require 'dotenv'
 require 'yaml'
 require 'hashie'
 require 'pagerduty'
@@ -14,6 +13,22 @@ require 'rspec/core/formatters/documentation_formatter'
 require 'rspec/core/formatters/html_formatter'
 
 module RScout
+  # DEFAULT_LOGGER  = Logger.new($stdout)
+  # DEFAULT_OPTIONS = {
+  #   logger: DEFAULT_LOGGER,
+  #   severity: Logger::Severity::DEBUG
+  # }
+
+  # class << self
+  #   def options
+  #     @@options ||= DEFAULT_OPTIONS.clone
+  #   end
+
+  #   def log(msg)
+  #     options[:logger].add(options[:severity]) { msg }
+  #   end
+  # end
+
   def self.logger
     @@LOGGER ||= begin
       logger = Logger.new STDOUT
@@ -22,66 +37,16 @@ module RScout
     end
   end
 
-  def self.load(options={})
-    Dotenv.load
+  def self.run_suite(dir, env)
+    verbose = false
+    gemfile = File.join(dir, 'Gemfile')
+    configfile = File.join(dir, 'config', 'rscout.yml')
+    dotfile = File.join(dir, '.env')
 
-    @@ENVS ||= begin
-      envs = options[:envs] ? Hashie::Mash.new(YAML.load_file(options[:envs])) : []
-
-      raise "No environments found." unless envs.keys
-
-      envs
-    end
-
-    @@SUITES ||= begin
-      suites = options[:suites] ? Hashie::Mash.new(YAML.load_file(options[:suites])) : []
-
-      raise "No suites found." unless suites.keys
-
-      suites
-    end
-  end
-
-  def self.envs(env_names=nil)
-    if env_names
-      envs = {}
-      env_names.each do |env_name|
-        raise "Unknown environment '#{env_name}'." unless @@ENVS[env_name]
-        envs[env_name] = @@ENVS[env_name]
-      end
-      envs
-    else
-      @@ENVS
-    end
-  end
-
-  def self.suites(suite_names=nil)
-    if suite_names
-      suites = {}
-      suite_names.each do |suite_name|
-        raise "Unknown suite '#{suite_name}'." unless @@SUITES[suite_name]
-        suites[suite_name] = @@SUITES[suite_name]
-      end
-      suites
-    else
-      @@SUITES
-    end
-  end
-
-  def self.run(options={}, &block)
-    verbose = options[:verbose] || false
-    RScout.logger.level = verbose ? Logger::DEBUG : Logger::ERROR
-    logger.debug "Running…"
-    RScout.envs(options[:envs].map(&:to_sym)).each do |env_name, env_config|
-      RScout.logger.debug "Running tests for #{env_name}…"
-      env_config.name = env_name
-      $target_env = env_config
-      RScout.suites(options[:suites].map(&:to_sym)).each do |suite_name, suite_config|
-        suite_config.name = suite_name
-        $target_suite = suite_config
-        RScout.logger.debug "Running tests for #{suite_name} on #{env_name}…"
-
-        yield if block_given?
+    Bundler.with_clean_env do
+      Dir.chdir(dir) do
+        yaml = Hashie::Mash.new(YAML.load_file configfile)
+        config = yaml[env]
 
         output = Hashie::Mash.new({
           txt: StringIO.new,
@@ -104,7 +69,9 @@ module RScout
           # RSpec::Core::Runner.disable_autorun!
           rspec.instance_variable_set(:@reporter, reporter)
 
-          rspec_task = lambda { RSpec::Core::Runner.run(Dir["#{ENV['RSCOUT_SUITE_DIR']}/#{suite_name}/*.rb"]) }
+          tests = Dir.glob File.join(dir, 'spec/**/*_spec.rb')
+
+          rspec_task = lambda { RSpec::Core::Runner.run tests }
 
           if verbose
             rspec_task.call
@@ -129,23 +96,23 @@ module RScout
 
         if failed
           RScout.logger.info "Tests failed."
-          RScout.send_failure_notifications env_config, suite_config, output
+          RScout.send_failure_notifications config, env, output
         end
+
+        failed
       end
     end
-  rescue => e
-    RScout.logger.fatal e.message
   end
 
-  def self.send_failure_notifications(env, suite, output)
+  def self.send_failure_notifications(config, env, output)
     email_body = [output.txt.string, output.error.backtrace.join("\n")].join("\n")
-    if env.email_enabled && suite.email
-      RScout.logger.info "Sending emails alert to #{suite.email}"
+    if config.email_enabled && config.email
+      RScout.logger.info "Sending emails alert to #{config.email}"
       begin
         mail = Mail.new do
           from     'Scout <platform+scout@evertrue.com>'
-          to       suite.email
-          subject  "Scout Alert: Tests failing on #{suite.name.to_s.humanize.titleize} (#{env.name.downcase})"
+          to       config.email
+          subject  "Scout Alert: Tests failing on #{config.name.to_s.humanize.titleize} (#{env})"
           add_file filename: 'results.html', content: output.html.string
 
           header["X-Priority"] = "1 (Highest)"
@@ -164,20 +131,20 @@ module RScout
       end
     end
 
-    if env.pagerduty_enabled && suite.pagerduty_service_key
-      RScout.logger.info "Triggering PagerDuty incident to #{suite.pagerduty_service_key}"
+    if config.pagerduty_enabled && config.pagerduty_service_key
+      RScout.logger.info "Triggering PagerDuty incident to #{config.pagerduty_service_key}"
       begin
-        if suite.pagerduty_service_key.match(/@(.*)pagerduty.com$/)
+        if config.pagerduty_service_key.match(/@(.*)pagerduty.com$/)
           mail = Mail.new do
              from    'RScout <platform+rscout@evertrue.com>'
-             to      suite.pagerduty_service_key
-             subject "DOWN alert: RScout tests failing on #{suite.name.to_s.humanize.titleize} (#{env.name.downcase})"
+             to      config.pagerduty_service_key
+             subject "DOWN alert: RScout tests failing on #{config.name.to_s.humanize.titleize} (#{env})"
              body    email_body
           end
 
           mail.deliver!
         else
-          p = Pagerduty.new suite.pagerduty_service_key, ['scout', env.name].join('_')
+          p = Pagerduty.new config.pagerduty_service_key, ['scout', env].join('_')
           incident = p.trigger 'RScout tests failing!', output.results
         end
       rescue => e
--- rscout.gemspec
+++ rscout.gemspec
@@ -29,6 +29,7 @@ Gem::Specification.new do |spec|
   spec.add_dependency 'mail'
   spec.add_dependency 'activesupport', '~> 4.0', '>= 4.0.2'
 
+  spec.add_development_dependency "byebug"
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
 end
